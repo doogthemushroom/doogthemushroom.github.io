ff1bc4ee886b9f8f33341ec2ae6a2ceae779030a
Douglas Williams <williams.douglas.m@gmail.com>
2020-10-22 22:03:43 -0500

    Updates

---
+++ precache-manifest.a6f2311aa00699f5e33d62616b1c53d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6876270a052cabbee17f3f4b3b11637",
+    "url": "/index.html"
+  },
+  {
+    "revision": "944c88cb591f8f6773b2",
+    "url": "/static/css/main.e86d31fb.chunk.css"
+  },
+  {
+    "revision": "1c500f6b53d5ac532e0b",
+    "url": "/static/js/2.0b291c68.chunk.js"
+  },
+  {
+    "revision": "44ba416b9a7217ca99c025d9f4dd1124",
+    "url": "/static/js/2.0b291c68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "944c88cb591f8f6773b2",
+    "url": "/static/js/main.f64a51ff.chunk.js"
+  },
+  {
+    "revision": "73a4f215652a07459b34",
+    "url": "/static/js/runtime-main.3bbf3189.js"
+  }
+]);
